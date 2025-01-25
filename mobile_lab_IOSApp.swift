71731bbc6aca4757f06ab76cb6084cb394ace35f
Egor123qwe <egorrrrrrrsdg@mail.ru>
2025-01-25 21:01:27 +0300

    init commit

---
+++ mobile_lab_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_lab_IOSApp.swift
+//  mobile-lab-IOS
+//
+//  Created by Igor Tryhan on 25.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct mobile_lab_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
